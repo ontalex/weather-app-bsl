aaf0927b91886aca249ea6bb09edd2ff7ae074e5
Алексей Б <loja.bobaev2000@yandex.ru>
2024-12-04 14:38:03 +0300

    Add: анимация загрузки для приложения

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import LoadingState from './components/Loading/LoadingState.vue';
 
 </script>
 
@@ -7,11 +8,20 @@ import { RouterView } from 'vue-router';
   <Suspense>
     <RouterView />
     <template #fallback>
-      <p>
-        Загрузка данных...
-      </p>
+      <div class="wrapper">
+        <LoadingState />
+      </div>
     </template>
   </Suspense>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.wrapper {
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
